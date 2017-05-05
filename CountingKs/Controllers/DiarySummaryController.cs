162ba362d325cc596e62047bcf9346859da91226
Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-05-05 17:28:59 +0700

    In this part we made a RemoteProcedure style call to the webAPI.
    we started with a DiarySummaryController which had a get method to return the summary of that day.
    
    We then added the route neede to reach this controller in the webApiConfig file.
    
    Finally we implemented the CreateSummary method on the ModelFactory.
    
    The createsummary basicalle takes in the diary of an user and computes the total calorie consumption of the userfor that day.
    
    I guess the use of this would be to let the server do the processing to find out how many calories have been eaten, rather than doing that one the client.
    
    Seems fairly straightforward to me :)

---
+++ CountingKs/Controllers/DiarySummaryController.cs
@@ -0,0 +1,41 @@
+﻿using CountingKs.Data;
+using CountingKs.Models;
+using CountingKs.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountingKs.Controllers
+{
+    public class DiarySummaryController : BaseApiController
+    {
+        private ICountingKsIdentityService _identityService;
+
+        public DiarySummaryController(ICountingKsRepository repo, ICountingKsIdentityService identity) :base(repo)
+        {
+            _identityService = identity;
+        }
+
+        public object Get(DateTime diaryid)
+        {
+            try
+            {
+                var diary = TheRepository.GetDiary(_identityService.CurrentUser, diaryid);
+
+                if(diary == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+
+                return TheModelFactory.CreateSummary(diary);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e.ToString());
+            }
+        }
+    }
+}
