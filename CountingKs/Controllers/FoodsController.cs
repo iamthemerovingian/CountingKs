e198881043c171ead99da74a0fd66fda25dbf78e
Milinda <Milinda@MILLSURFACEPRO3>
2017-05-07 17:57:11 +0700

    Implemented basic user authorization using the buiilt in aps.net authorization.

--- CountingKs/Controllers/FoodsController.cs
+++ CountingKs/Controllers/FoodsController.cs
@@ -14,6 +14,7 @@ using System.Web.Http.Routing;
 namespace CountingKs.Controllers
 {
     //[RequireHttps]
+    [Authorize]
     public class FoodsController : BaseApiController
     {
         const int PAGE_SIZE = 50;
--- CountingKs/Services/CountingKsIdentityService.cs
+++ CountingKs/Services/CountingKsIdentityService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 
 namespace CountingKs.Services
@@ -11,7 +12,8 @@ namespace CountingKs.Services
         {
             get
             {
-                return "shawnwildermuth";
+                var name = Thread.CurrentPrincipal.Identity.Name;
+                return name;
             }
             //set
             //{
